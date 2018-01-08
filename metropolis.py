9e3c6093c9c490b8ed514699ff528de05640aaaf
ROCHE SHANE <shroche@tcd.ie>
2018-01-08 11:28:12 +0000

    added working metropolis algorithm

---
+++ metropolis.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+import random
+import numpy as np
+def metropolis(lattice,kT,J,h):
+    "metropolis algorithm for updating lattice"
+    N=len(lattice)
+    for i in range(N):
+        for j in range(N):
+            neighbours= lattice[i,(j+1)%N] + lattice[i,(j-1)%N] + lattice[(i+1)%N,j] + lattice[(i-1)%N,j]
+            delta_H = (-2*lattice[i,j])*(J*neighbours + h)
+            if delta_H>=0:
+                lattice[i,j]=-lattice[i,j]
+            elif random.random()<np.exp((delta_H)/kT):
+                lattice[i,j]=-lattice[i,j]
+    return lattice
