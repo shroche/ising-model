dccab629b4db838c2e85000e9b70507c136f5fee
ROCHE SHANE <shroche@tcd.ie>
2018-01-12 10:18:33 +0000

    improved the commenting of the code

--- metropolis.py
+++ metropolis.py
@@ -1,12 +1,22 @@
 #!usr/bin/env python
+
+
+"importing relevant packages"
+
 import random
 import numpy as np
+
+
+
+
+"metropolis algorithms for lattices of different dimensions"
+
 def metropolis(lattice,kT,J,h):
-    "metropolis algorithm for updating lattice"
+    "Performs one sweep of the metropolis algorithm for the 2D lattice"
     N=len(lattice)
     for i in range(N):
         for j in range(N):
-            neighbours= lattice[i,(j+1)%N] + lattice[i,(j-1)%N] + lattice[(i+1)%N,j] + lattice[(i-1)%N,j]
+            neighbours= lattice[i,(j+1)%N] + lattice[i,(j-1)%N] + lattice[(i+1)%N,j] + lattice[(i-1)%N,j] # modular gives periodicity
             delta_H = (-2*lattice[i,j])*(J*neighbours + h)
             if delta_H>=0:
                 lattice[i,j]=-lattice[i,j]
@@ -16,6 +26,7 @@ return lattice
 
 
 def metropolis_3D(lattice,kT,J,h):
+    "Performs one sweep of the metropolis algorithm for the 3D lattice"
     N=len(lattice)
     for i in range(N):
         for j in range(N):
@@ -28,7 +39,9 @@ def metropolis_3D(lattice,kT,J,h):
                     lattice[i,j,k]=-lattice[i,j,k]
     return lattice
 
+
 def metropolis_1D(lattice,kT,J,h):
+    "Performs one sweep of the metropolis algorithm for the 1D lattice"
     N=len(lattice)
     for i in range(N):
                 neighbours=lattice[(i+1)%N]+lattice[(i-1)%N]
@@ -40,16 +53,12 @@ def metropolis_1D(lattice,kT,J,h):
     return lattice
 
 
-def convert_to_pbm(lattice):
 
-    for i in range(len(lattice)):
-        for j in range(len(lattice)):
-            if lattice[i,j]==-1:
-                lattice[i,j]=0
 
+"Functions that find energy per lattice point of lattices of different dimensions"
 
 def find_energy(lattice,J,h):
-    "finding the energy per lattice point"
+    "finding the energy per lattice point for 2D lattice"
     N=len(lattice)
     energy=0.0
     for i in range(N):
@@ -57,10 +66,11 @@ def find_energy(lattice,J,h):
             s=lattice[i,j]
             neighbours= lattice[i,(j+1)%N] + lattice[i,(j-1)%N] + lattice[(i+1)%N,j] + lattice[(i-1)%N,j]
             energy=energy-(J*neighbours*s)-4*h*s
-    return float(energy/(2*N*N))
+    return float(energy/(2*N*N)) #divide by 2 to overcome double counting
 
 
 def find_energy_3D(lattice,J,h):
+    "finding the energy per lattice point for 3D lattice"
     N=len(lattice)
     energy=0.0
     for i in range(N):
@@ -69,35 +79,45 @@ def find_energy_3D(lattice,J,h):
                 s=lattice[i,j,k]
                 neighbours= lattice[i,(j+1)%N,k] + lattice[i,(j-1)%N,k] + lattice[(i+1)%N,j,k] + lattice[(i-1)%N,j,k]+ lattice[i,j,(k+1)%N] + lattice[i,j,(k-1)%N]
                 energy=energy-(J*neighbours*s)-4*h*s
-    return float((energy/(2*N*N*N)))
+    return float((energy/(2*N*N*N))) #divide by 2 to overcome double counting
 
-    
+   
 def find_energy_1D(lattice,J,h):
+    "finding the energy per lattice point for the 1D lattice
     N=len(lattice)
     energy=0.0
     for i in range(N):
         s=lattice[i]
         neighbours=lattice[(i+1)%N]+lattice[(i-1)%N]
         energy=energy-(J*neighbours*s)-h*s
-    return float((energy/(2*N)))
+    return float((energy/(2*N))) #divide by 2 to overcome double counting
 
 
+
+
+"Functions that find the magnetization"
 
 def find_magnetization(lattice):
-    "finding average magnetization per site"
+    "finding magnetization for 2D lattice"
     return abs(float(np.sum(lattice))/(len(lattice)*len(lattice)))
 
+
 def find_magnetization_3D(lattice):
-    "finding average magnetization per site"
+    "finding magnetization for 3D lattice"
     return abs(float(np.sum(lattice))/(len(lattice)*len(lattice)*len(lattice)))
 
 
-
 def find_magnetization_1D(lattice):
-    "finding average magnetization per site"
+    "finding magnetization for 1D lattice"
     return abs(float(np.sum(lattice))/(len(lattice)))
 
 
 
+"Miscellaneous functions"
 
-
+def convert_to_pbm(lattice):
+    "Puts matrix into pbm format, spin down=white cell, spin up=black cell"
+    for i in range(len(lattice)):
+        for j in range(len(lattice)):
+            if lattice[i,j]==-1:
+                lattice[i,j]=0
